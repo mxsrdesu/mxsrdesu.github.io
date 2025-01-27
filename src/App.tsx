8442acb2ecfa294016cc042655129941dfe9080d
Maxim <h11d3nname@gmail.com>
2025-01-27 19:57:24 +0300

    feat: add GitHub Pages deployment workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Prompt from './components/Prompt';
+import Header from './components/Header/Header';
+import Footer from './components/Footer';
+import './App.css';
 
 function App() {
-  const [count, setCount] = createSignal(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
+      <Prompt />
+      <Header />
+      <main>
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
