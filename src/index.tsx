b72798f34ee4c1ae5654802e9743b3ea7c5747f5
Maxim <h11d3nname@gmail.com>
2025-02-06 04:12:40 +0300

    biome fix some stupid shit

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,8 @@ import { render } from 'solid-js/web';
 import App from './components/App';
 
 const root = document.getElementById('root');
+if (!root) {
+  throw new Error('No element with id "root" found');
+}
 
-render(() => <App />, root!);
+render(() => <App />, root);
